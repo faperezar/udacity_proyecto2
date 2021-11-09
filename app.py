f8f2382b99a89d879402795c3171ca0f629ad0cd
Fernando Perez <faperezar@achs.cl>
2021-11-08 22:38:35 -0300

    App.py modificacion agregando prediccion

--- app.py
+++ app.py
@@ -1,9 +1,80 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+#Este arrojaba error "from sklearn.preprocessing import StandardScaler"
+import joblib
+
+#Todo esto es para el levantamiento de una web simple con Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    html = "<h3>Udacity - Sklearn Prediction Home</h3>"
+    return html.format(format)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+#De aca en adelante es el servicio de prediccion
+
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+
+def scale(payload):
+    """Scales Payload"""
+
+    LOG.info("Scaling Payload: %s payload")
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    """Performs an sklearn prediction
+
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+
+    result looks like:
+    { "prediction": [ 20.35373177134412 ] }
+
+    """
+
+    try:
+        clf = joblib.load("boston_housing_prediction.joblib")
+    except:
+        LOG.info("JSON payload: %s json_payload")
+        return "Model not loaded"
+
+    json_payload = request.json
+    LOG.info("JSON payload: %s json_payload")
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info("inference payload DataFrame: %s inference_payload")
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    return jsonify({'prediction': prediction})
+
+#Ojo, esto lo deberia poder probar desde POSTMAN 
+#o ejecutando el make_prediction.sh para probar el publicado en el local
+#o el make_prediction_azure_app.sh para probar el publicado en la webapp
